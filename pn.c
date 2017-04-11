2c5a898857d97c854572fa1ed2f14a45ffeb2e43
sathyasubramani <sathya.seee@ksrce.ac.in>
2017-04-11 14:37:41 +0530

    Create pn.c

---
+++ pn.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+int num;
+printf("print the value");
+scanf("%d",&num);
+if(num>0)
+printf("positive");
+else if("num<0")
+printf("negative");
+else
+printf("zero");
+return o;
+}
